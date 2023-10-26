f03d24038b4b277a2bbb80f4ea20b9dcee0c0ae4
Gorock Shetty <gorock397@gmail.com>
2023-10-26 11:22:12 +1100

    made changes to the api call

--- server.js
+++ server.js
@@ -2,28 +2,27 @@ const PORT = 8000
 const express = require('express')
 const cors = require('cors')
 const app = express()
+
 app.use(express.json())
 app.use(cors())
+require('dotenv').config();
 
-const API_KEY = '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'
+
+const API_KEY = process.env.OPENAI_API_KEY
 
 app.post('/completions', async (req, res) => {
 
     const options = {
         method:"POST",
         headers: {
-            "Authorization": 'Bearer ${API_KEY}',
+            "Authorization": `Bearer ${API_KEY}`,
             "Content-Type": "application/json"
         },
-
-
-        
-
 
         body: JSON.stringify({
             model : "gpt-3.5-turbo",
             messages: [{role: "user", content: req.body.message}],
-            max_tokens: 7,
+            max_tokens: 100,
         })
     }
 
@@ -31,11 +30,9 @@ app.post('/completions', async (req, res) => {
         const response = await fetch('https://api.openai.com/v1/chat/completions', options)
         const data = await response.json()
         res.send(data)
-
     }
     catch (error) { 
         console.error(error)
-
     }
 }
 )
--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './Components/Assets/logo.png'
 import './App.css';
 import { Login_Signup } from './Components/Login-Signup/Login-Signup';
 
@@ -18,205 +17,164 @@ const App = () => {
     setCurrentTitle(null)
   }
 
-
   const handleClick = (uniqueTitles) => {
     setCurrentTitle(uniqueTitles)
     setMessage(null)
     setValue("")
   }
 
-
-
-  // const getMessages = async () => {
-
-  //   const options = {
-  //     method: "POST",
-  //     body : JSON.stringify({
-  //       message: value
-  //     }),
-  //     headers: {
-  //       "Content-Type": "application/json"
-  //     }
-
-  //   }
-  //   try{
-  //     const response = await fetch('http://localhost:8000/completions', options)
-  //     const data = await response.json()
-
-  //     setMessage(data.choices[0].messages)
-  //   }catch (error){
-  //     console.error(error)
-  //   }
-  // }
-
-
-
   const getMessages = async () => {
     const options = {
       method: "POST",
-      body: JSON.stringify({
-        message: value,
+      body : JSON.stringify({
+        message: value
       }),
       headers: {
-        "Content-Type": "application/json",
-      },
-    };
-  
-    try {
-      const response = await fetch('http://localhost:8000/completions', options);
-      const data = await response.json();
-  
-      if (data.choices && data.choices.length > 0) {
-        setMessage(data.choices[0].messages);
-      } else {
-        // Handle the case where there are no choices in the response.
-        // You might want to set a default message or handle it accordingly.
-        setMessage('No response available');
+        "Content-Type": "application/json"
       }
-    } catch (error) {
-      console.error(error);
     }
-  };
+    try{
+      const response = await fetch('http://localhost:8000/completions', options)
+      const data = await response.json()
+      setMessage(data.choices[0].message)
+    }catch (error){
+      console.error(error)
+    }
+  }
+
+
+  // const getMessages = async () => {
+  //   const options = {
+  //     method: "POST",
+  //     body: JSON.stringify({
+  //       message: value,
+  //     }),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     }
+  //   }
+  
+  //   try {
+  //     const response = await fetch('http://localhost:8000/completions', options);
+
+  //     if (!response.ok) {
+  //       throw new Error('Network response was not ok');
+  //     }
+
+  //     const data = await response.json();
+  
+  //     if (data.choices && data.choices.length > 0) {
+  //       setMessage(data.choices[0].messages);
+  //     } else {
+  //       // Handle the case where there are no choices in the response.
+  //       // You might want to set a default message or handle it accordingly.
+  //       setMessage('No response available');
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
   
 
-
-
-  // useEffect(() => {
-  //   console.log(currentTitle, value, message)
-  //   if(!currentTitle && value && message){
-  //     setCurrentTitle(value)
-  //   }
-
-  //   if(currentTitle && value && message){
-  //     setPreviousChats(prevChats => (
-  //       [...prevChats, 
-  //        {
-  //         title: currentTitle,
-  //         role: "user",
-  //         content: value
-  //        },
-         
-  //        {
-  //         title :currentTitle,
-  //         role: message.role,
-  //         content: message.content
-  //         }
-  //       ]
-  //     ))
-  //   }
-  // },[message, currentTitle])
-
-
-
- 
-
   useEffect(() => {
-    // This effect runs when 'message' changes.
-    console.log(currentTitle, value, message);
-    if (!currentTitle && value && message) {
-      setCurrentTitle(value);
+    console.log(currentTitle, value, message)
+    if(!currentTitle && value && message){
+      setCurrentTitle(value)
     }
-  }, [message]);
-  
-  useEffect(() => {
-    // This effect runs when 'currentTitle' changes.
-    if (currentTitle && value && message) {
-      setPreviousChats((prevChats) => [
-        ...prevChats,
-        {
+
+    if(currentTitle && value && message){
+      setPreviousChats(prevChats => (
+        [...prevChats, 
+         {
           title: currentTitle,
           role: "user",
-          content: value,
-        },
-        {
-          title: currentTitle,
+          content: value
+         },
+         
+         {
+          title :currentTitle,
           role: message.role,
-          content: message.content,
-        },
-      ]);
+          content: message.content
+          }
+        ]
+      ))
     }
-  }, [currentTitle, value, message]);
-  
-
-
-
-
+  },[message, currentTitle])
   console.log(previousChats)
+
+  // useEffect(() => {
+  //   // This effect runs when 'message' changes.
+  //   console.log(currentTitle, value, message);
+  //   if (!currentTitle && value && message) {
+  //     setCurrentTitle(value);
+  //   }
+  // }, [message]);
+  
+  // useEffect(() => {
+  //   // This effect runs when 'currentTitle' changes.
+  //   if (currentTitle && value && message) {
+  //     setPreviousChats((prevChats) => [
+  //       ...prevChats,
+  //       {
+  //         title: currentTitle,
+  //         role: "user",
+  //         content: value,
+  //       },
+  //       {
+  //         title: currentTitle,
+  //         role: message.role,
+  //         content: message.content,
+  //       },
+  //     ]);
+  //   }
+  // }, [currentTitle, value, message]);
+  
+  // console.log(previousChats)
 
   const currentChat = previousChats.filter(previousChats => previousChats.title === currentTitle)
   const uniqueTitles = Array.from(new Set(previousChats.map(previousChats => previousChats.title)))
-  console.log(uniqueTitles)
-
+ 
   return (
-    <div >
-      <Login_Signup/>
+    <><Login_Signup /><div>
+      <div className='app'>
+         {/* we can create a chat component itself, it starts here*/}
 
-
-    <div className="app">
-      {/* we can create a chat component itself, it starts here*/}
-
-      {/* creating a side bar */}
-      <section className='side-bar'>
-        <button onClick={createNewChat}> + New Chat</button>
-
-        {/* <ul className='history'>
-
-          {uniqueTitles?.map((uniqueTitles, index) => <li key={index} onClick={handleClick(uniqueTitles)}>{uniqueTitles}</li>)}
-        </ul> */}
-
-        <ul className='history'>
-          {uniqueTitles?.map((uniqueTitle, index) => (
-            <li key={index} onClick={() => handleClick(uniqueTitle)}>
-              {uniqueTitle}
-            </li>
-          ))}
-        </ul>
-
-
-        <nav>
-          <p>Logout</p>
-        </nav>
-      </section>
-
-
-
-      {/* creating a main bar */}
-      <section className='main'>
-        {!currentTitle && <h1>CodePilot</h1>} 
-        <ul className='feed'>
-          {currentChat?.map((chatMessage, index) => <li key={index}>
-            <p className='role'>{chatMessage.role}</p>
-            <p>{chatMessage.content}</p>
-
-          </li>)}
-
-        </ul>
-
-        <div className='bottom-section'>
-          <div className='input-container'>
-
-
-            {/* <input value={value} onChange={(e) => setValue(e.target.value)}/> */}
-            <input value={value || ''} onChange={(e) => setValue(e.target.value)} />
-
-           
-           
-            <div id='submit' onClick={getMessages}>➤</div>
+        {/* creating a side bar */}
+        <section className='side-bar'>
+          <button onClick={createNewChat}>+ New chat</button>
+          <ul className='history'>
+            {uniqueTitles?.map((uniqueTitle, index) => (
+              <li key={index} onClick={() => handleClick(uniqueTitle)}>
+                {uniqueTitle}
+              </li>
+            ))}
+          </ul>
+          <nav>
+            <p>Logout</p>
+          </nav>
+        </section>
+                {/* creating a main bar */}
+        <section className='main'>
+          {!currentTitle && <h1>CodePilot</h1>}
+          <ul className='feed'>
+            {currentChat?.map((chatMessage, index) => <li key={index}>
+              <p className='role'>{chatMessage.role}</p>
+              <p>{chatMessage.content}</p>
+            </li>)}
+          </ul>
+          <div className='botton-section'>
+            <div className='input-container'>
+              <input value={value} onChange={(e) => setValue(e.target.value)} />
+              <div id='submit' onClick={getMessages}>➤</div>
+            </div>
+            <p className='info'>
+              Copyright CodePilot 2023
+            </p>
           </div>
+        </section>
 
-          <p className='info'>
-            Copyright CodePilot 2023
-          </p>
-        </div>
-      </section>
-      
-
-
-    </div>
-    {/* chat component ends here */}
-
-
-    </div>
+      </div>
+    </div></>
   );
 }
 
